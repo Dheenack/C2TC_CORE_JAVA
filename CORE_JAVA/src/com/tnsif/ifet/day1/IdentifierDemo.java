dfcfb10ddda8ff4b3bde0d6ed15a6b98b3beb98b
Dheena Krishna <dheenakrishna2020@gmail.com>
2025-09-29 22:01:14 +0530

    New changes according to day 4 lecture...

---
+++ CORE_JAVA/src/com/tnsif/ifet/day1/IdentifierDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.ifet.day1;
+
+public class IdentifierDemo {
+    public static void main(String[] args) {
+        int $num=23;
+        String Name="Dheena Krishna";
+        System.out.println("Name: "+Name+"\nNumber: "+$num);
+    
+    }
+}
--- CORE_JAVA/src/day2/DoWhileDemo.java
+++
@@ -1,9 +0,0 @@
-package day2;
-
-public class DoWhileDemo {
-    public static void main(String[] args) {
-        do{
-            System.out.println("Hello");
-        }while(false);
-    }
-}
